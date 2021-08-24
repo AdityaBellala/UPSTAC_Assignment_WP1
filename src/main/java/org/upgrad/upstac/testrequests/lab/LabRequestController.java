b26fc5eff61db8efa1148e55d09e67980732555e
AdityaBellala <adityabellala@hotmail.com>
2021-08-24 23:21:38 +0530

    Updated getForTester function

--- src/main/java/org/upgrad/upstac/testrequests/lab/LabRequestController.java
+++ src/main/java/org/upgrad/upstac/testrequests/lab/LabRequestController.java
@@ -67,7 +67,7 @@ public class LabRequestController {
 
 
         User tester =userLoggedInService.getLoggedInUser();
-        return testRequestQueryService.findByUser(tester);
+        return testRequestQueryService.findByTester(tester);
 
 
 
